42b44d4562aba835be18687688f04dc8119394e4
Tamas Aczima <tamas.aczima@bossastudios.com>
2018-05-29 12:32:42 +0100

    Added allergens for customers

--- Assets/Scripts/Classes/Allergens.cs
+++ Assets/Scripts/Classes/Allergens.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 
-namespace Assets.Scripts.Classes {
-    [Serializable]
-    public enum Allergens {
-        Peanuts,
-        TreeNuts,
-        Shellfish,
-        Fish,
-        Eggs,
-        Lactose,
-        Gluten,
-        Soya,
-        SesameSeeds,
-        Celery,
-        Mustard,
-    }
+[Serializable]
+public enum Allergens {
+    Peanuts,
+    TreeNuts,
+    Shellfish,
+    Fish,
+    Eggs,
+    Lactose,
+    Gluten,
+    Soya,
+    SesameSeeds,
+    Celery,
+    Mustard,
 }
