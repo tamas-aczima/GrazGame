f6c997ece2d6dfe7b0aa1da918b045902f3fb117
Emina Prlja <emina.prlja.1993@outlook.com>
2018-05-31 15:08:56 +0200

    main menu improvement + music

---
+++ Assets/MainMenu/MainMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("Bootstrap");
+
+    }
+
+    public void OnClickQuit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
